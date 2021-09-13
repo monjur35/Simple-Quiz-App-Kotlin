29e56c12d4679f0b4d9c83ff127446b23ec30999
monjur35 <monjur35-1019@diu.edu.bd>
2021-09-14 00:05:17 +0600

    nav_graph implemented

---
+++ app/src/main/java/com/example/quiz/NameFragment.kt
@@ -0,0 +1,36 @@
+package com.example.quiz
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.quiz.databinding.FragmentNameBinding
+
+class NameFragment : Fragment() {
+
+     lateinit var  binding : FragmentNameBinding
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding= FragmentNameBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.startBtn.setOnClickListener{
+            Toast.makeText(requireContext(),"Clicked",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+}
